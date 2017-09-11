7a3ce6a017b66b9f00ee58d9457e526e76802524
Arkadiusz Dzięgiel <arkadiusz.dziegiel@glorpen.pl>
2017-09-11 10:37:24 +0200

    more variables, changed some paths, added puppet cache cleanup

--- centos/opt/share/common.sh
+++ centos/opt/share/common.sh
@@ -5,15 +5,18 @@
 set -e
 
 puppetizer_sources="/opt/puppetizer/sources"
-puppetizer_modules="/var/opt/puppetizer/modules"
+puppetizer_var_dir="/var/opt/puppetizer"
+puppetizer_modules="${puppetizer_var_dir}/modules"
 vendor_modules="/var/opt/puppetizer/vendor"
 puppetizer_init="/var/opt/puppetizer/init.pp"
+puppet_conf_dir="/etc/puppetlabs/puppet"
+puppet_env_dir="/etc/puppetlabs/code/environments"
 
 function setup_puppetizer_modules()
 {
 	echo "" > "${puppetizer_init}"
 
-	cat << EOF > /etc/puppetlabs/puppet/hiera.yaml	
+	cat << EOF > "${puppet_conf_dir}/hiera.yaml"
 ---
 # Hiera 5 Global configuration file
 
@@ -22,7 +25,7 @@ version: 5
 hierarchy:
   - name: Puppetizer runtime data
     data_hash: yaml_data
-    path: /var/opt/puppetizer/hiera/runtime.yaml
+    path: ${puppetizer_var_dir}/hiera/runtime.yaml
 EOF
 	
 	find "${puppetizer_sources}" -maxdepth 1 -mindepth 1 -type d | while read n;
@@ -32,13 +35,13 @@ EOF
 		ln -fs "${n}/code" "${puppetizer_modules}/${module_name}";
 		echo "include ::${module_name}" >> "${puppetizer_init}"
 		
-		cat << EOF >> /etc/puppetlabs/puppet/hiera.yaml
+		cat << EOF >> "${puppet_conf_dir}/hiera.yaml"
   - name: Puppetizer env data for ${module_name}
     data_hash: yaml_data
-    path: /opt/puppetizer/sources/${short_name}/hiera/env-%{environment}.yaml
+    path: ${puppetizer_sources}/${short_name}/hiera/env-%{environment}.yaml
   - name: Puppetizer data for ${module_name}
     data_hash: yaml_data
-    path: /opt/puppetizer/sources/${short_name}/hiera/puppetizer.yaml
+    path: ${puppetizer_sources}/${short_name}/hiera/puppetizer.yaml
 EOF
 		
 	done
