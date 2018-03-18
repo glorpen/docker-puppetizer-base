e80660737cfcb07a1b3ef4852f416ef1d1f8214a
Arkadiusz Dzięgiel <arkadiusz.dziegiel@glorpen.pl>
2018-03-18 13:16:16 +0100

    remove test echo

--- ci/build.sh
+++ ci/build.sh
@@ -4,5 +4,5 @@
 
 for tag in ${REPO_TAGS};
 do
-	echo docker build --build-arg IMAGE_VERSION="${tag}" -t "${REPO_NAME}":"${tag}" .
+	docker build --build-arg IMAGE_VERSION="${tag}" -t "${REPO_NAME}":"${tag}" .
 done
