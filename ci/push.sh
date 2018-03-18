556de5848037dd6e1d93aa6fc327d9831679b919
Arkadiusz Dzięgiel <arkadiusz.dziegiel@glorpen.pl>
2018-03-18 21:17:57 +0100

    travis: docker login & push

--- ci/push.sh
+++ ci/push.sh
@@ -1,7 +1,12 @@
-
 #
 # author: Arkadiusz Dzięgiel <arkadiusz.dziegiel@glorpen.pl>
 #
 
-#docker login -u "${DOCKER_USER}" -p "${DOCKER_PASSWORD}"
-#docker push "${REPO_NAME}"
+docker login -u "${DOCKER_USER}" -p "${DOCKER_PASSWORD}"
+
+for tag in ${REPO_TAGS};
+do
+	name="${REPO_NAME}:${tag}"
+	echo "Pushing ${name}"
+	docker push "${name}"
+done
