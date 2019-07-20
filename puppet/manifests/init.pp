b1e00ff532d09ec39e5529cd174199b92a05c0b0
Arkadiusz Dzięgiel <arkadiusz.dziegiel@glorpen.pl>
2019-07-20 13:55:53 +0200

    disable backup for files by default

--- puppet/manifests/init.pp
+++ puppet/manifests/init.pp
@@ -1,4 +1,9 @@
 class puppetizer {
   $puppetizer_var = '/opt/puppetizer'
   $health_scripts_path = "${puppetizer_var}/health"
+
+  # no need for backups inside container
+  File <| |> {
+    backup => false
+  }
 }
